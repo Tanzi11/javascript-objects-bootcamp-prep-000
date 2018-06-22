a6ceb37e5fe293538e1addb03813eba243702dd7
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-06-22 07:42:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,6 @@ function updatePlaylist (playlist, artist, song) {
   playlist[artist] = song;
   return playlist;
 }
-function removeFromPlaylist (playlist, artist) {}
+function removeFromPlaylist (playlist, artistName) {
+  delete
+}
