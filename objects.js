64c540e64988a3078c60ecad2817f2d2da780104
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-06-22 07:41:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = new Object ({artist: 'song title'});
 function updatePlaylist (playlist, artist, song) {
-  playlist[artist] = song
-  return playlist
+  playlist[artist] = song;
+  return playlist;
 }
